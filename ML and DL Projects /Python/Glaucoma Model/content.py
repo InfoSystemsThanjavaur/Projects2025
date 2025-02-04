905945203115c84fe47bcbee450c46efb3867a03
InfoSystemsThanjavaur <150105991+InfoSystemsThanjavaur@users.noreply.github.com>
2025-02-04 15:41:13 +0530

    Add files via upload

---
+++ ML and DL Projects /Python/Glaucoma Model/content.py
@@ -0,0 +1,64 @@
+eye_conditions = {
+    0: {
+        "name": "cataract",
+        "definition": "A condition where the lens of the eye becomes cloudy, leading to blurred or impaired vision.",
+        "cause": "Aging, diabetes, prolonged exposure to sunlight, smoking, or genetics.",
+        "symptoms": [
+            "Blurry vision",
+            "Difficulty seeing at night",
+            "Sensitivity to light",
+            "Fading or yellowing of colors"
+        ],
+        "treatment": [
+            "Glasses or magnifying lenses (early stages)",
+            "Cataract surgery (advanced stages)"
+        ]
+    },
+    1: {
+        "name": "diabetic retinopathy",
+        "definition": "A complication of diabetes that affects the retina, caused by damage to the blood vessels in the eye.",
+        "cause": "Chronic high blood sugar levels.",
+        "symptoms": [
+            "Blurry vision",
+            "Dark spots or floaters",
+            "Difficulty seeing colors",
+            "Vision loss in severe cases"
+        ],
+        "treatment": [
+            "Blood sugar control",
+            "Laser surgery to seal leaking blood vessels",
+            "Medications like anti-VEGF injections"
+        ]
+    },
+    2: {
+        "name": "glaucoma",
+        "definition": "A group of eye conditions that damage the optic nerve, often due to increased pressure in the eye.",
+        "cause": "Blocked drainage canals, genetics, or other eye conditions.",
+        "symptoms": [
+            "Loss of peripheral (side) vision",
+            "Tunnel vision in advanced stages",
+            "Eye pain, nausea, or headaches (in acute cases)"
+        ],
+        "treatment": [
+            "Eye drops to reduce eye pressure",
+            "Laser therapy or surgery for drainage improvement",
+            "Oral medications in severe cases"
+        ]
+    },
+   3: {
+    "name": "normal",
+    "definition": "Refers to a healthy eye with no visible signs of disease or abnormality.",
+    "cause": "No disease or abnormalities; the eye is functioning as intended.",
+    "symptoms": [
+        "Clear and unobstructed vision",
+        "No pain or discomfort in the eye",
+        "Proper response to light stimuli"
+    ],
+    "treatment": [
+        "No treatment required for a healthy eye",
+        "Regular preventive measures like eye check-ups and UV protection",
+        "Maintaining a balanced diet rich in essential vitamins (A, C, E)"
+    ]
+}
+
+}
